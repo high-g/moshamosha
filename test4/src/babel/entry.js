908666cfba350e1c5695f32380ad8b637504ef68
1−10tani <tani@1-10.com>
2019-03-05 20:02:34 +0900

    [update]

--- test4/src/babel/entry.js
+++
@@ -1,6 +0,0 @@
-const navBtn = document.getElementById('header-navBtn');
-const header = document.getElementById('header');
-
-navBtn.addEventListener('click', (event) => {
-  header.classList.toggle('open');
-});
--- test5/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: 'development',
-  entry: "./src/js/index.js",
-  output: {
-    path: `${__dirname}/dist/assets/js`,
-    filename: "main.js"
-  }
-};
