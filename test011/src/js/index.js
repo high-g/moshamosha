7db99c8ba950e7632cf2d109fc9a5b2581196a82
tani <tani@1-10.com>
2020-07-09 00:13:39 +0900

    [update]motion graphics

--- test011/src/js/index.js
+++ test011/src/js/index.js
@@ -2,12 +2,22 @@ import $ from 'jquery'
 import 'velocity-animate'
 
 $(function() {
+  $('#wrapper').removeAttr('style')
+
   const init = () => {
-    $('#line1').addClass('on')
+    const num_last1 = 3000
+
+    setTimeout(() => {$('#line1').addClass('on') }, 0);
     setTimeout(() => {$('#line2').addClass('on') }, 200);
     setTimeout(() => {$('#line3').addClass('on') }, 400);
-    setTimeout(() => {$('#text1').addClass('on') }, 550);
-    setTimeout(() => {$('#scene1').addClass('on') }, 2000);
+    setTimeout(() => {$('#text1').addClass('on') }, 600);
+    setTimeout(() => {$('#text2').addClass('on') }, 1600);
+    setTimeout(() => {$('#scene1').addClass('on') }, num_last1);
+
+    setTimeout(() => {$('#s2ImageWrap').addClass('on') }, 1500 + num_last1);
+    setTimeout(() => {$('#s2Image1').addClass('on') }, 1500 + num_last1);
+    setTimeout(() => {$('#s2Blue').addClass('on') }, 2000 + num_last1);
+    setTimeout(() => {$('#s2Rect').addClass('on') }, 2000 + num_last1);
   }
 
   setTimeout(init, 500)
